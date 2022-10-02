1a536cc47606f569a0e3c7948ab0358e79adc04e
Gabriel <gabriel@MacBook.local>
2022-10-01 23:27:33 -0700

    Aula 15 - metodos 2

---
+++ Aulas_GoLang/15 - Métodos/metodos2.go/metodos2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type consoles struct {
+	Marca   string
+	Modelo  string
+	Valor   int
+	BlueRay bool
+}
+
+func (c consoles) salvar() {
+	fmt.Printf("O console %v, no valor de R$ %v ,foi salvo no banco com exito", c.Modelo, c.Valor)
+}
+
+func (c consoles) desconto() {
+	total := c.Valor * 10 / 100
+	total2 := c.Valor - total
+	fmt.Printf("\nO valor do %s é %v Reais, para pagamento a vista você terá um desconto de %v, saindo pelo valor de %v Reais", c.Modelo, c.Valor, total, total2)
+}
+
+func main() {
+
+	PS5 := consoles{"Sony", "PS5", 5785, true}
+	PS5.salvar()
+	PS5.desconto()
+
+}
