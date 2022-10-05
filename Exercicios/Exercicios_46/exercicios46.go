74a21670354ab77f8c51e09bcbfc9ff7f12e06f3
Gabriel <gabriel@MacBook.local>
2022-10-05 00:42:48 -0700

    Exercicios 46

---
+++ Exercicios/Exercicios_46/exercicios46.go
@@ -0,0 +1,34 @@
+// - Partindo do código abaixo, utilize unmarshal e demonstre os valores.
+//     - https://play.golang.org/p/b_UuCcZag9
+// - Dica: JSON-to-Go.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type jsontogo []struct {
+	First   string   `json:"First"`
+	Last    string   `json:"Last"`
+	Age     int      `json:"Age"`
+	Sayings []string `json:"Sayings"`
+}
+
+func main() {
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+
+	var resultado jsontogo
+
+	err := json.Unmarshal([]byte(s), &resultado)
+	if err != nil {
+		fmt.Println("deu zica mano!", err)
+	}
+
+	fmt.Println(resultado)
+	fmt.Println(resultado[1])
+	fmt.Println(resultado[1].Last)
+
+}
