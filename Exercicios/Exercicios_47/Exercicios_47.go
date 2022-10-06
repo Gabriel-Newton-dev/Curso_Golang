0c21632fd2e7f020ba79d094d636c9ec6a33b5d0
Gabriel <gabriel@MacBook.local>
2022-10-05 23:50:22 -0700

    Package Sort

--- Exercicios/Exercicios_47/Exercicios_47.go
+++ Exercicios/Exercicios_47/Exercicios_47.go
@@ -1,6 +1,5 @@
 // Partindo do código abaixo, utilize NewEncoder() e Encode() para enviar as informações como JSON para Stdout.
 // - https://play.golang.org/p/BVRZTdlUZ_
-// - Desafio: descubra o que é, e utilize, method chaining para conectar os dois métodos acima.
 
 package main
 
