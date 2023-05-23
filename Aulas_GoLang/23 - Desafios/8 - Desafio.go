4f34a51671ac7b192ab2a6fb2eccd52b5b68368a
Gabriel <gabriel@MacBook.local>
2023-05-23 01:01:06 -0700

    [FEAT] Criando tabuada

---
+++ Aulas_GoLang/23 - Desafios/8 - Desafio.go
@@ -0,0 +1,17 @@
+// Como calcular a tabuada de multiplicação
+// para os números de 1 a 9 em Golang - Exercícios Resolvidos de Java
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("      Tabuada de Multiplicação    ")
+	fmt.Println("----------------------------------------------")
+
+	for i := 1; i <= 9; i++ {
+		fmt.Printf("%d |\n", i)
+	}
+
+}
